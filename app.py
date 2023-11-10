a307c10a7b410c590cc9948ec6346a8a0d108120
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-11-10 19:05:10 +0530

    Initial V1 Commit

---
+++ app.py
@@ -0,0 +1,56 @@
+import streamlit as st
+import openai
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Set your OpenAI API key
+openai.api_key = os.getenv("OPENAI_API_KEY", "default_key")
+
+# Function to generate content using GPT-4
+def generate_content(mood, type, subject, date, description, tags):
+    prompt = (
+        f"I want you to draft me a {mood} LinkedIn post. It is based on "
+        f"{type}. The following are the details: {type} {subject} "
+        f"{date} {description}. You can include more similar tags like {tags}"
+    )
+
+    response = openai.Completion.create(
+        engine="text-davinci-003",  # You can experiment with different engines
+        prompt=prompt,
+        max_tokens=200  # Adjust based on your needs
+    )
+    return response.choices[0].text.strip()
+
+# Page title
+st.title("LinkedIn Post Generator - LinkScript")
+
+# User inputs
+subject = st.text_input("Subject:")
+description = st.text_area("Description:")
+date = st.date_input("Date:")
+mood = st.selectbox("Mood of Writing:", ["Happy", "Neutral", "Professional"])
+type = st.selectbox("Type:", ["Article", "Event", "Job", "Achievement", "Other"])
+tags = st.text_input("Tags (comma-separated):")
+
+# Button to generate post
+if st.button("Generate Post"):
+    # Use GPT-4 to generate content
+    generated_content = generate_content(mood, type, subject, date, description, tags)
+
+    # Combine inputs with generated content into a post
+    post_content = f"🚀 **{subject}**\n\n{description}\n\n📅 {date}\n\n😊 Mood: {mood}\n\nType: {type}\n\nGenerated Content: {generated_content}\n\nTags: {tags}"
+
+    # Display the generated post
+    st.success("Your LinkedIn Post is ready! Copy and paste the content below:")
+    st.code(post_content)
+
+# GitHub link
+st.markdown("[GitHub Repository](https://github.com/mruduljohn/LinkScript)")
+
+# Motivation
+st.markdown("## Motivation\n\nOur motivation is to simplify the creation of engaging LinkedIn content...")
+
+# Footer
+st.markdown("Let's code, contribute, and become Legend Gladiators together! 🚀")
