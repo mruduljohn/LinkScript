9ab972d03f0a500c95f679e61334f694d96e4c7c
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-11-10 21:13:50 +0530

    Fixed Logical Issues
    Added Emoji Support

--- app.py
+++ app.py
@@ -13,9 +13,11 @@ client = OpenAI(
 # Function to generate content using GPT-4
 def generate_content(mood, type, subject, date, description, tags):
     prompt = (
-        f"I want you to draft me a {mood} LinkedIn post. It is based on "
-        f"{type}. The following are the details: {type} {subject} "
-        f"{date} {description}. You can include more similar tags like {tags}"
+        f"I want you to act as a LinkedIn content writer."
+        f"You should draft me a {mood} LinkedIn post. It is an {type} about {subject} that happened on {date}. "
+        f"The following are the details: {type} {subject} "
+        f"{date} {description}. You may use emojis depending on the mood.  You can include more similar tags like {tags}." 
+        f"Tags should only be displayed at the end of the post.[NO TAGS at the beginning of the post]. It should follow a LinkedIn post format."
     )
 
     response = client.completions.create(
@@ -33,7 +35,7 @@ st.title("LinkedIn Post Generator - LinkScript")
 subject = st.text_input("Subject:")
 description = st.text_area("Description:")
 date = st.date_input("Date:")
-mood = st.selectbox("Mood of Writing:", ["Happy", "Neutral", "Professional,Cool", "Sad"])
+mood = st.selectbox("Mood of Writing:", ["Happy", "Neutral", "Professional","Cool", "Sad"])
 type = st.selectbox("Type:", ["Article", "Event", "Job", "Achievement", "Other"])
 #specify the typpe if other is selected
 if type == "Other":
@@ -46,11 +48,13 @@ if st.button("Generate Post"):
     generated_content = generate_content(mood, type, subject, date, description, tags)
 
     # Combine inputs with generated content into a post
-    post_content = f"🚀 **{subject}**\n\n{description}\n\n📅 {date}\n\n😊 Mood: {mood}\n\nType: {type}\n\nGenerated Content: {generated_content}\n\nTags: {tags}"
+    # post_content = f"🚀 **{subject}**\n\n{description}\n\n📅 {date}\n\n😊 Mood: {mood}\n\nType: {type}\n\nGenerated Content: {generated_content}\n\nTags: {tags}"
 
     # Display the generated post
     st.success("Your LinkedIn Post is ready! Copy and paste the content below:")
-    st.code(generated_content)
+    st.text_area("Generated Content", generated_content, height=300)
+    st.write("Click 'Generate Post' button again for a different result.")
+    st.markdown(" Note! Please remention the Profiles of the people and organizations involved in the post.")
 
 # GitHub link
 st.markdown("[GitHub Repository](https://github.com/mruduljohn/LinkScript)")
