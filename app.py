d1bc58b5e7c8729a1e6cad24bd221c29964b2b7e
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2023-11-10 21:27:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # Set your OpenAI API key
 client = OpenAI(
-  api_key=st.secrets[OPENAI_API_KEY],  # this is also the default, it can be omitted
+  api_key=st.secrets['OPENAI_API_KEY'],  # this is also the default, it can be omitted
   #api_key=os.environ.get("OPENAI_API_KEY")
 )
 
